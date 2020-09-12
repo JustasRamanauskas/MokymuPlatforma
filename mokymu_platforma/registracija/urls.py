6130d76b2d1c13e8765ac05e2613ed7cedc16dfe
JustasRamanauskas <justas.ramanauskas@versloarchitektai.lt>
2020-09-12 13:32:39 +0300

    Front_end dalis

--- mokymu_platforma/registracija/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from mokymu_platforma.registracija.views import registracija
-
-urlpatterns = [
-    path('registracija/', registracija),
-]

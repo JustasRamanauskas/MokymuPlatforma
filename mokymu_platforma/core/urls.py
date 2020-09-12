699471d9981e12c1feb7e3b950ea92450e5cdf32
TheSliumba <asliumbaz@gmail.com>
2020-09-12 13:45:08 +0300

    added login.html to empty path

--- mokymu_platforma/core/urls.py
+++ mokymu_platforma/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from mokymu_platforma.core.views import registracija
+from mokymu_platforma.core.views import login
 
 urlpatterns = [
     path('registracija/', registracija),
+    path('', login)
 ]

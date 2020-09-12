fb1e52c4a078a0dbb9dfb12ebc2062be61b26e32
JustasRamanauskas <justas.ramanauskas@versloarchitektai.lt>
2020-09-12 14:36:54 +0300

    User dalis

---
+++ mokymu_platforma/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+
+class User(models.Model):
+    login_name = models.CharField(max_length=200)
+    login_password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    photo = models.ImageField()
+    user_created = models.TimeField (blank=False, null=False, default=datetime.datetime.now())
+    other = models.CharField(max_length=200)
+
+class Roles(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE())
+    roll_type = models.CharField(max_length=200)
+

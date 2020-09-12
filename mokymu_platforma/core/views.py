384bfec5d2b299c05c622c7b82209a6864e90b39
TheSliumba <asliumbaz@gmail.com>
2020-09-12 14:12:12 +0300

    fixed /registracija link in index.html

--- mokymu_platforma/core/views.py
+++ mokymu_platforma/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def registracija(request):
-    return render(request, "index.html")
+    return render(request, "registracija.html")
 
 def login(request):
     return render(request, "login.html")

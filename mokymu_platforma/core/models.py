91090c9ea8a0146ff3b60b51136a7d06eba8722c
RafaelAchmedov <rafael.achmedov@gmail.com>
2020-09-12 15:39:08 +0300

    suvesti visi modeliai(reikia patikrinti)

--- mokymu_platforma/core/models.py
+++ mokymu_platforma/core/models.py
@@ -5,40 +5,6 @@ import datetime
 
 # Create your models here.
 
-class Company(models.Model):
-    company_name = models.CharField(max_length=20)
-    company_logo = models.ImageField()
-    company_description = models.TextField()
-
-class Course(models.Model):
-    course_category = models.CharField(max_length=20)
-    course_description = models.TextField()
-
-class Courses_Company(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
-    company_id = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
-
-
-class Student(models.Model):
-    user_id = models.CharField(max_length=20)
-    student_personalCode = models.CharField(max_length=20)
-    registration_date = models.DateTimeField(default=datetime.datetime.now())
-    delete_date = models.DateTimeField(blank=True, null=True)
-    status = models.BooleanField(default=False)
-
-class CoursesGroup_Student(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
-    company_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
-
-class Courses_Group(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
-    course_teacher_id = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
-
-
-
-
-
-
 class User(models.Model):
     login_name = models.CharField(max_length=200)
     login_password = models.CharField(max_length=200)
@@ -52,3 +18,94 @@ class User(models.Model):
 class Roles(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     roll_type = models.CharField(max_length=200)
+
+class Company(models.Model):
+    role_id = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    company_name = models.CharField(max_length=20)
+    company_logo = models.ImageField()
+    company_description = models.TextField()
+
+class Course(models.Model):
+    role_id = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    course_category = models.CharField(max_length=20)
+    course_description = models.TextField()
+
+class Courses_Company(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    company_id = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+
+
+class Student(models.Model):
+    role_id = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    student_personalCode = models.CharField(max_length=20)
+    registration_date = models.DateTimeField(default=datetime.datetime.now())
+    delete_date = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+
+class CoursesGroup_Student(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    company_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+
+class Teacher(models.Model):
+    role_id = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    rating = models.IntegerField()
+    #other
+
+class Pricing(models.Model):
+    course_id = models.CharField(max_length=20)
+    discount = models.IntegerField()
+    from_date = models.DateTimeField(blank=True, null=True)
+    till_date = models.DateTimeField(blank=True, null=True)
+
+class Courses_Group(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    course_teacher_id = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    course_group_description = models.TextField()
+    course_group_price = models.IntegerField()
+    course_price = models.IntegerField()
+    course_pricing_id = models.ForeignKey(Pricing, on_delete=models.DO_NOTHING)
+
+class Client(models.Model):
+    role_id = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    company_name = models.CharField(max_length=20)
+    company_code = models.CharField(max_length=20)
+    company_address = models.CharField(max_length=50)
+
+class Contract_Student(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    client_id = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+    course_group_id = models.ForeignKey(Courses_Group, on_delete=models.DO_NOTHING)
+    price = models.IntegerField()
+    payment_method = models.CharField(max_length=20)
+    payment_delay = models.IntegerField
+
+class Schedule_Course(models.Model):
+    course_group_id = models.ForeignKey(Courses_Group, on_delete=models.DO_NOTHING)
+    start_date = models.DateTimeField(blank=True, null=True)
+    finish_date = models.DateTimeField(blank=True, null=True)
+    start_theory_date = models.DateTimeField(blank=True, null=True)
+    finish_theory_date = models.DateTimeField(blank=True, null=True)
+    start_practice_date = models.DateTimeField(blank=True, null=True)
+    finish_practice_date = models.DateTimeField(blank=True, null=True)
+    exam_date = models.DateTimeField(blank=True, null=True)
+
+class Student_Finish_Courses(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    finish_date = models.DateTimeField(blank=True, null=True)
+    valid_until_date = models.DateTimeField(blank=True, null=True)
+    theory_result = models.IntegerField()
+    practical_result = models.ImageField()
+
+
+
+
+
+
+
+
+
+
+
+
+

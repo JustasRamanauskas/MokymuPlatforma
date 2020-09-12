f6494823e61ec75b9c5ff2b8c8bb00002cce2d0e
TheSliumba <asliumbaz@gmail.com>
2020-09-12 13:45:08 +0300

    added login.html to empty path

--- mokymu_platforma/core/views.py
+++ mokymu_platforma/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def registracija(request):
     return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")

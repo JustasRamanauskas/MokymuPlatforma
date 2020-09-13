1a8498223c035495673f597698dd9df5146b95c7
JustasRamanauskas <justas.ramanauskas@versloarchitektai.lt>
2020-09-13 10:53:47 +0300

    Registracija login tvarkoma POST metodas ir veikia kad enable ir disenable button kai password nesutampa

--- mokymu_platforma/core/views.py
+++ mokymu_platforma/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mokymu_platforma.core.models import User
+
+
 def registracija(request):
+    if request.method == "POST":
+        user = User()
+        user.login_name = request.POST['inputEmailAddress']
+        if request.POST['inputPassword'] == request.POST['inputPassword2']:
+            user.login_password = request.POST['inputEmailAddress']
+
     return render(request, "registracija.html")
 
 def login(request):
